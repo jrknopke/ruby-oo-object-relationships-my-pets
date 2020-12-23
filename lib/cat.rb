5dfd11c836ecbf82b8188249bfcc51bb20a0cf0f
Joseph Knopke <josephknopke@gmail.com>
2020-12-23 00:00:11 -0600

    commit tree 2429b8c7aef2b7a539a323a5ac65573b14a3bd44

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Cat
   # code goes here
+  attr_accessor :owner, :cat, :mood
+  attr_reader :name
+
+  @@cats = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    #binding.pry
+    @mood = "nervous"
+    @@cats << self
+  end
+
+  def mood
+    @mood
+  end
+
+  def self.all
+    @@cats
+  end
+
 end
